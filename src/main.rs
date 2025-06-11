ad6c9d418a88a6238af6440a024adb0133710389
sndmnska <102878282+sndmnska@users.noreply.github.com>
2025-06-11 14:40:54 -0500

    basic high card logic implimented, start on pair

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![allow(dead_code)]
 
 use std::cmp::Ordering;
+use std::collections::HashMap;
 
 // mod structenum;
 
@@ -84,7 +85,7 @@ fn parse_hand(s: &str) -> Hand {
 
     let mut cards = s.split_whitespace().map(parse_card).collect::<Vec<_>>();
     cards.sort_by_key(|card| card.rank);
-    cards.reverse();
+    cards.reverse(); // sort from A to 2, opposite order from above
 
     // let cards = vec![
     //     parse_card(&s[0..2]),
@@ -113,7 +114,7 @@ enum Suit {
     Spades,
 }
 
-#[derive(Debug, Copy, Clone, PartialEq, Eq, PartialOrd, Ord)]
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
 enum Rank {
     Two,
     Three,
@@ -130,24 +131,44 @@ enum Rank {
     Ace,
 }
 
-#[derive(Debug, PartialEq, Eq)]
-enum Outcome {
-    Player1,
-    Player2,
-    Draw,
-}
+fn compare_hands(hand1: &Hand, hand2: &Hand) -> Ordering {
+    // Check for pairs
+    //
+    let is_pair_h1 = detect_pair(hand1);
+    let is_pair_h2 = detect_pair(hand2);
 
-// TODO Maybe replace outcome with Ordering?
+    // if is_pair_h1 && is_pair_h2 {
+    // } else if is_pair_h1 {
+    //     return Ordering::Greater;
+    // } else if is_pair_h2 {
+    //     return Ordering::Less;
+    // } else {
+    //     //
+    // }
 
-fn compare_hands(hand1: &Hand, hand2: &Hand) -> Outcome {
+    // TODO Write a test to check what happens if there's three of a kind on this....
+    // TODO Eventually end up with a bunch of functions check for these things
+
+    match (is_pair_h1, is_pair_h2) {
+        (true, true) => {
+            // TODO compare the card in the pair???
+
+            // TODO if that card is the same then compare the remaining
+            // cards for high card?
+        }
+        (true, false) => {
+            return Ordering::Greater;
+        }
+        (false, true) => {
+            return Ordering::Less;
+        }
+        _ => {}
+    }
+
     let ranks1 = hand1.cards.iter().map(|card| card.rank).collect::<Vec<_>>();
     let ranks2 = hand2.cards.iter().map(|card| card.rank).collect::<Vec<_>>();
 
-    match ranks1.cmp(&ranks2) {
-        Ordering::Greater => Outcome::Player1,
-        Ordering::Less => Outcome::Player2,
-        Ordering::Equal => Outcome::Draw,
-    }
+    ranks1.cmp(&ranks2) // returns result of comparison (as an Ordering)
 
     // What the above does:
     // if ranks1 > ranks2 {
@@ -159,40 +180,90 @@ fn compare_hands(hand1: &Hand, hand2: &Hand) -> Outcome {
     // }
 }
 
+fn detect_pair(hand: &Hand) -> bool {
+    // TODO is there a pair, and option of the rank coming through, and
+    // returning a vector of the cards not in the pair
+
+    let mut counts: HashMap<Rank, u8> = HashMap::new();
+
+    // do the counting here
+    for card in &hand.cards {
+        let rank = card.rank;
+        counts
+            .entry(rank)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+        // let count = counts.get(&rank);
+        // if let Some(count) = count {
+        //     counts.insert(rank, count + 1);
+        // } else {
+        //     counts.insert(rank, 1);
+        // }
+    }
+
+    for (_rank, count) in counts {
+        if count == 2 {
+            return true;
+        }
+    }
+    false
+    // true
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn test_compare_poker_hands() {
-        // Example hand:
-        //
-        // 2H 3D 5S 9C KD   2C 3H 4S 8C AH
-        // Player 2 wins.
+        // Hand 2 wins.
         let hand1 = parse_hand("2H 3D 5S 9C KD");
         let hand2 = parse_hand("2C 3H 4S 8C AH");
-        assert_eq!(compare_hands(&hand1, &hand2), Outcome::Player2);
+        assert_eq!(compare_hands(&hand1, &hand2), Ordering::Less);
     }
 
     #[test]
     fn test_compare_poker_hands2() {
-        // Example hand:
-        //
-        // 2H 3D 5S 9C KD   2C 3H 4S 8C AH
-        // Player 2 wins.
+        // Hand 1 wins.
         let hand1 = parse_hand("2C 3H 4S 8C AH");
         let hand2 = parse_hand("2H 3D 5S 9C KD");
-        assert_eq!(compare_hands(&hand1, &hand2), Outcome::Player1);
+        assert_eq!(compare_hands(&hand1, &hand2), Ordering::Greater);
     }
 
+    #[test]
     fn test_compare_poker_hands_draw() {
-        // Example hand:
-        //
-        // 2H 3D 5S 9C KD   2C 3H 4S 8C AH
-        // Player 2 wins.
+        // Draw
         let hand1 = parse_hand("2C 3H 4S 8C AH");
         let hand2 = parse_hand("2H 3C 4H 8S AC");
-        assert_eq!(compare_hands(&hand1, &hand2), Outcome::Draw);
+        assert_eq!(compare_hands(&hand1, &hand2), Ordering::Equal);
+    }
+
+    #[test]
+    fn test_compare_poker_hands_second_high_card() {
+        // Hand 2 wins
+        let hand1 = parse_hand("2C 3H 4S 8C AH");
+        let hand2 = parse_hand("2H 3D 5S 9C AD");
+        assert_eq!(compare_hands(&hand1, &hand2), Ordering::Less);
+    }
+
+    #[test]
+    fn test_detect_pair_true() {
+        let hand: Hand = parse_hand("2C 3H 4S 8C 8H");
+        assert!(detect_pair(&hand));
+    }
+
+    #[test]
+    fn test_detect_pair_false() {
+        let hand: Hand = parse_hand("2C 3H 4S 8C 9H");
+        assert!(!detect_pair(&hand));
+    }
+
+    #[test]
+    fn test_pair_poker_hand1() {
+        // Hand 1 wins
+        let hand1: Hand = parse_hand("2C 3H 4S 8C 8H");
+        let hand2: Hand = parse_hand("2H 3D 5S 9H KH");
+        assert_eq!(compare_hands(&hand1, &hand2), Ordering::Greater);
     }
 
     #[test]
